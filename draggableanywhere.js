117650c86757e83faf1140cef03a4e34b3ad1763
MathEnthusiast314 <89387162+MathEnthusiast314@users.noreply.github.com>
2022-06-05 22:33:05 -0500

    Update draggableanywhere.js

--- draggableanywhere.js
+++ draggableanywhere.js
@@ -1,3 +1,4 @@
+//Example: https://www.desmos.com/calculator/rmhphnvkhw. Drag the 3d graph inside the rectangle;)
 state=Calc.getState() 
 state.expressions.list.push({"type":"image","id":"dragme","image_url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2P4////fwAJ+wP9BUNFygAAAABJRU5ErkJggg==","name":"Seamless dragging, courtesy of @ayunami2000!!!","foreground":true,"center":"C_{4}","width":"10^{5}","height":"10^{5}","opacity":"0","draggable":true})
 Calc.setState(state, {allowUndo: true});
