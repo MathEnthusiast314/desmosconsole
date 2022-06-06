440891eec1ee65afcc1d6e93578877bd517386f5
MathEnthusiast314 <89387162+MathEnthusiast314@users.noreply.github.com>
2022-06-05 22:00:03 -0500

    Create draggableanywhere.js

---
+++ draggableanywhere.js
@@ -0,0 +1,3 @@
+state=Calc.getState() 
+state.expressions.list.push({"type":"image","id":"dragme","image_url":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVQYV2P4////fwAJ+wP9BUNFygAAAABJRU5ErkJggg==","name":"Seamless dragging, courtesy of @ayunami2000!!!","foreground":true,"center":"C_{4}","width":"10^{5}","height":"10^{5}","opacity":"0","draggable":true})
+Calc.setState(state, {allowUndo: true});
